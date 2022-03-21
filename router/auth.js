5dc5aa6f3b8c2d9246df7888d6e1e0e60c62f212
Nihat Aydın <08200000296@ogrenci.ege.edu.tr>
2022-03-22 00:06:02 +0300

    commit on master

--- router/auth.js
+++ router/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -38,9 +39,18 @@ router.post("/login", async (req, res) => {
     Originalpassword !== req.body.password &&
       res.status(401).json("wrong credentials!");
 
+    const accesToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT.SEC,
+      { expiresIn: "3d" }
+    );
+
     const { password, ...others } = user._doc;
 
-    res.status(200).json(others);
+    res.status(200).json({ ...others, accesToken });
   } catch (err) {
     res.status(500).json(err);
   }

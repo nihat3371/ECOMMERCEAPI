e703bf19378a89e5bd294fdb22b7c2699c33c899
Nihat Aydın <08200000296@ogrenci.ege.edu.tr>
2022-03-25 21:53:23 +0300

    commit on master

--- router/auth.js
+++ router/auth.js
@@ -44,7 +44,7 @@ router.post("/login", async (req, res) => {
         id: user._id,
         isAdmin: user.isAdmin,
       },
-      process.env.JWT.SEC,
+      process.env.JWT_SEC,
       { expiresIn: "3d" }
     );
 

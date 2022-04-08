933cad82d5c1de8feef9fa64ceec395ff2748044
Nihat Aydın <08200000296@ogrenci.ege.edu.tr>
2022-04-08 15:23:46 +0300

    product add done

--- router/product.js
+++ router/product.js
@@ -10,7 +10,7 @@ const router = require("express").Router();
 //CREATE
 
 router.post("/", verifyTokenAndAdmin, async (req, res) => {
-  const newProduct = newProduct(req.body);
+  const newProduct = new Product(req.body);
 
   try {
     const savedProduct = await newProduct.save();

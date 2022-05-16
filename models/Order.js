35d0038e67154d5b5a78fcaaf97179fcc176ac08
Nihat Aydın <08200000296@ogrenci.ege.edu.tr>
2022-05-17 00:27:54 +0300

    Some dumb mistakes fixed

--- models/Order.js
+++ models/Order.js
@@ -15,7 +15,7 @@ const OrderSchema = new mongoose.Schema(
       },
     ],
     amount: { type: Number, required: true },
-    addres: { type: Object, required: true },
+    address: { type: Object, required: true },
     status: { type: String, default: "pending" },
   },
 

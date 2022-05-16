983f0edab5e80081f5cce0a635ecfe02edd1ce2c
Nihat Aydın <08200000296@ogrenci.ege.edu.tr>
2022-05-17 00:42:53 +0300

    SECOND ROOKIE MISTAKE LOL

--- router/order.js
+++ router/order.js
@@ -72,7 +72,7 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
 
 // GET MONTHLY INCOME
 
-router.get("income", verifyTokenAndAdmin, async (req, res) => {
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
   const date = new Date();
   const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
   const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
